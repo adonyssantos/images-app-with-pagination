c957d6118554ec178405981ed4fd11f8dc73e3b6
adonyssantos <adonyssantosjimenez@gmail.com>
2025-04-01 22:28:33 -0400

    Add PhotoCard component to encapsulate photo display and deletion logic

---
+++ src/components/PhotoCard.tsx
@@ -0,0 +1,66 @@
+import {
+  Button,
+  Grid,
+  Card,
+  CardMedia,
+  CardActions,
+  Typography,
+  Dialog,
+  DialogTitle,
+  DialogContentText,
+  DialogActions,
+  DialogContent,
+} from "@mui/material";
+import type { Photo } from "../models/rovers.service";
+import { useState } from "react";
+
+type PhotoCardProps = {
+  photo: Photo;
+  handleDelete: (id: number) => void;
+};
+
+export function PhotoCard({ photo, handleDelete }: PhotoCardProps) {
+  const [open, setOpen] = useState(false);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <Grid key={photo.id}>
+      <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
+        <CardMedia
+          component="img"
+          sx={{
+            height: 300,
+            width: 300,
+            objectFit: "cover",
+          }}
+          image={photo.img_src}
+          alt={`Mars Rover Photo ${photo.id}`}
+        />
+        <CardActions sx={{ mt: "auto" }}>
+          <Typography variant="caption" sx={{ flexGrow: 1 }}>
+            ID: {photo.id}
+            <br />
+            Camera: {photo.camera.name}
+            <br />
+            Earth Date: {photo.earth_date}
+            <br />
+            Rover: {photo.rover.name}
+          </Typography>
+          <Button size="small" color="error" onClick={() => setOpen(true)}>
+            Delete
+          </Button>
+        </CardActions>
+      </Card>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Delete Photo</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Are you sure you want to delete this photo?</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={() => handleDelete(photo.id)}>Delete</Button>
+        </DialogActions>
+      </Dialog>
+    </Grid>
+  );
+}
--- src/components/PhotosGrid.tsx
+++ src/components/PhotosGrid.tsx
@@ -1,18 +1,6 @@
 import { Photo } from "../models/rovers.service";
-import {
-  Button,
-  Grid,
-  Card,
-  CardMedia,
-  CardActions,
-  Typography,
-  Dialog,
-  DialogTitle,
-  DialogContentText,
-  DialogActions,
-  DialogContent,
-} from "@mui/material";
-import { useState } from "react";
+import { Grid } from "@mui/material";
+import { PhotoCard } from "./PhotoCard";
 
 type PhotosGridProps = {
   photos: Photo[];
@@ -20,50 +8,10 @@ type PhotosGridProps = {
 };
 
 export function PhotosGrid({ photos, handleDelete }: PhotosGridProps) {
-  const [open, setOpen] = useState(false);
-  const handleClose = () => setOpen(false);
-
   return (
     <Grid container spacing={3} justifyContent="center">
       {photos.map((photo) => (
-        <Grid key={photo.id}>
-          <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
-            <CardMedia
-              component="img"
-              sx={{
-                height: 300,
-                width: 300,
-                objectFit: "cover",
-              }}
-              image={photo.img_src}
-              alt={`Mars Rover Photo ${photo.id}`}
-            />
-            <CardActions sx={{ mt: "auto" }}>
-              <Typography variant="caption" sx={{ flexGrow: 1 }}>
-                ID: {photo.id}
-                <br />
-                Camera: {photo.camera.name}
-                <br />
-                Earth Date: {photo.earth_date}
-                <br />
-                Rover: {photo.rover.name}
-              </Typography>
-              <Button size="small" color="error" onClick={() => setOpen(true)}>
-                Delete
-              </Button>
-            </CardActions>
-          </Card>
-          <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>Delete Photo</DialogTitle>
-            <DialogContent>
-              <DialogContentText>Are you sure you want to delete this photo?</DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleClose}>Cancel</Button>
-              <Button onClick={() => handleDelete(photo.id)}>Delete</Button>
-            </DialogActions>
-          </Dialog>
-        </Grid>
+        <PhotoCard key={photo.id} photo={photo} handleDelete={handleDelete} />
       ))}
     </Grid>
   );
